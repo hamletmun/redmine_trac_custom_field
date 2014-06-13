807f4216ed74be60c29cfa5d0cc95c0a14d3893c
hamletmun <hamletmun@gmail.com>
2014-06-13 16:23:10 -0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,33 +1,34 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_rt_custom_field do
-  name 'RT num to ticket'
-  author 'Steve Morrissey'
-  url 'https://github.com/uberamd/redmine_rt_custom_field/' if respond_to?(:url)
-  description 'Allows linking of a field to a RT ticket by linking to that ticket'
+Redmine::Plugin.register :redmine_trac_custom_field do
+  name 'Trac num to ticket'
+  author 'Seung Soo Mun'
+  url 'https://github.com/hamletmun/redmine_trac_custom_field/' if respond_to?(:url)
+  description 'A custom field that links your Redmine issue to Trac ticket number'
   version '0.1.1'
 
   requires_redmine :version_or_higher => '0.9.0'
 
-  settings :partial => 'settings/redmine_rt_custom_field_settings',
+  settings :partial => 'settings/redmine_trac_custom_field_settings',
     :default => {
-      'rt_url' => 'http://path.to.rt.com/',
+      'trac_url' => 'http://path.to.trac.com/',
+      'trac_project' => 'project_name',
       'new_window' => 'true', 
     }
 end
 
-class RtCustomFieldFormat < Redmine::CustomFieldFormat
+class TracCustomFieldFormat < Redmine::CustomFieldFormat
   include ActionView::Helpers::TextHelper
   include ActionView::Helpers::TagHelper
 
-  def format_as_rt(value)
-    if Setting.plugin_redmine_rt_custom_field['new_window'] == "true"
+  def format_as_trac(value)
+    if Setting.plugin_redmine_trac_custom_field['new_window'] == "true"
       target = 'blank'
     else
       target = ''
     end
     
-    ActionController::Base.helpers.link_to(value, Setting.plugin_redmine_rt_custom_field['rt_url'] + "Ticket/Display.html?id=" + value, :target => target)
+    ActionController::Base.helpers.link_to(value, Setting.plugin_redmine_trac_custom_field['trac_url'] + "projects/" + Setting.plugin_redmine_trac_custom_field['trac_project'] + "/ticket/" + value, :target => target)
   end
 
   def escape_html?
@@ -40,5 +41,5 @@ class RtCustomFieldFormat < Redmine::CustomFieldFormat
 end
 
 Redmine::CustomFieldFormat.map do |fields|
-  fields.register RtCustomFieldFormat.new('rt', :label => :label_rt, :order => 8)
+  fields.register TracCustomFieldFormat.new('trac', :label => :label_trac, :order => 8)
 end
